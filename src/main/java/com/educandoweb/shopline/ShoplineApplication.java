0b6b8b84bf0a50252cd7299b36be1ffb46585729
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-13 20:16:26 -0300

    project started

---
+++ src/main/java/com/educandoweb/shopline/ShoplineApplication.java
@@ -0,0 +1,12 @@
+package com.educandoweb.shopline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoplineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoplineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educandoweb/shopline/ShoplineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.shopline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoplineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
