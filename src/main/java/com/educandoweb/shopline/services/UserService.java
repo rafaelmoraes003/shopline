5892e244332d36e009f340c070035458714c7393
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-13 21:46:39 -0300

    FEAT: user service

---
+++ src/main/java/com/educandoweb/shopline/services/UserService.java
@@ -0,0 +1,24 @@
+package com.educandoweb.shopline.services;
+
+import com.educandoweb.shopline.entities.User;
+import com.educandoweb.shopline.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.get();
+    }
+}
