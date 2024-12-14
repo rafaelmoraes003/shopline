532b3c58d458bf57cc762c19e6914bf88600ada8
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-13 21:48:32 -0300

    FEAT: list all users

--- src/main/java/com/educandoweb/shopline/resources/UserResource.java
+++ src/main/java/com/educandoweb/shopline/resources/UserResource.java
@@ -1,18 +1,26 @@
 package com.educandoweb.shopline.resources;
 
 import com.educandoweb.shopline.entities.User;
+import com.educandoweb.shopline.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.HttpURLConnection;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService userService;
 
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "12982828145", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = this.userService.findAll();
+        return ResponseEntity.status(HttpURLConnection.HTTP_OK).body(users);
     }
 }
