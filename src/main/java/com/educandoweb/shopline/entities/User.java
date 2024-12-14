490eb218aae5a254556411b4dcac176c6a423bfc
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-14 12:49:26 -0300

    FEAT: one to many mapping added to user entity

--- src/main/java/com/educandoweb/shopline/entities/User.java
+++ src/main/java/com/educandoweb/shopline/entities/User.java
@@ -1,11 +1,10 @@
 package com.educandoweb.shopline.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity(name = "users")
@@ -17,6 +16,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -67,6 +69,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return this.orders;
     }
 
     @Override
