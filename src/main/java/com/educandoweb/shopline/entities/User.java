bb3988865b374f4938dffb6abb072091af869309
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-13 21:01:27 -0300

    FEAT: user entity for database mapping

--- src/main/java/com/educandoweb/shopline/entities/User.java
+++ src/main/java/com/educandoweb/shopline/entities/User.java
@@ -1,9 +1,17 @@
 package com.educandoweb.shopline.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity(name = "users")
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
