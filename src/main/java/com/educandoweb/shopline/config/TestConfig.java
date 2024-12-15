7a786b3ee96342454a0346161c94e3886874e0d4
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-14 21:47:42 -0300

    FEAT: seeding for categories table

--- src/main/java/com/educandoweb/shopline/config/TestConfig.java
+++ src/main/java/com/educandoweb/shopline/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.educandoweb.shopline.config;
 
+import com.educandoweb.shopline.entities.Category;
 import com.educandoweb.shopline.entities.Order;
 import com.educandoweb.shopline.entities.User;
 import com.educandoweb.shopline.entities.enums.OrderStatus;
+import com.educandoweb.shopline.repositories.CategoryRepository;
 import com.educandoweb.shopline.repositories.OrderRepository;
 import com.educandoweb.shopline.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +24,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        this.categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -33,5 +44,6 @@ public class TestConfig implements CommandLineRunner {
 
         this.userRepository.saveAll(Arrays.asList(u1, u2));
         this.orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
     }
 }
