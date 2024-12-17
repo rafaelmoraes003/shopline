b5737867bbe5cf8a752123e61c36d5c6dfdfff86
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-16 22:34:02 -0300

    FEAT: order item entity

---
+++ src/main/java/com/educandoweb/shopline/entities/OrderItem.java
@@ -0,0 +1,70 @@
+package com.educandoweb.shopline.entities;
+
+import com.educandoweb.shopline.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity(name = "order_items")
+public class OrderItem implements Serializable {
+    @EmbeddedId
+    private OrderItemPK id;
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        this.id.setOrder(order);
+        this.id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return this.id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        this.id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return this.id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        this.id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(this.id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.id);
+    }
+}
