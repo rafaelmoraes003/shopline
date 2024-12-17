1d0baa127cbe720063a2b5ae61fefdba62e018bd
rafaelmoraes003 <rafaelsm003@gmail.com>
2024-12-16 23:20:57 -0300

    FEAT: payment entity

---
+++ src/main/java/com/educandoweb/shopline/entities/Payment.java
@@ -0,0 +1,73 @@
+package com.educandoweb.shopline.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity(name = "payments")
+public class Payment implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return this.moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Order getOrder() {
+        return this.order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return Objects.equals(this.id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.id);
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "id=" + this.id +
+                ", moment=" + this.moment +
+                ", order=" + this.order +
+                '}';
+    }
+}
